d1bc1e720aefac5d60aa43413d53ea41947958ad
wishva <wishvasd1234@gmail.com>
2021-02-26 19:09:11 +0530

    add schema in projectors.model.js

---
+++ backend/modules/projectors.model.js
@@ -0,0 +1,31 @@
+// const { string } = require('@hapi/joi');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const projectorSchema = new Schema({
+
+    // roomName: {
+    //     type: String,
+    //     required: true,
+    //     min: 1,
+    //     max: 20
+    // },
+
+    controlUnitId:{
+        type: String,
+        required: true
+    },
+
+    isWorking: {
+        type: Boolean,
+        required: true
+    }
+
+}, {
+    timestamps: true
+});
+
+const Projector = mongoose.model('Projector', projectorSchema);
+
+module.exports = Projector;
