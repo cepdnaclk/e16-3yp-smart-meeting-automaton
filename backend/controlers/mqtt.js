038499d007adf3a47e5e93253d2731a8f8af5f24
wishva <wishvasd1234@gmail.com>
2021-03-07 21:15:40 +0530

    added function mqtt

--- backend/controlers/mqtt.js
+++ backend/controlers/mqtt.js
@@ -5,9 +5,61 @@ const mqtt = require('mqtt');
 // if (dotenv.error) {
 //     throw dotenv.error;
 // }
-function sendMqttNew({data, clientId, isretain = false, isclean = false, qosLevel = 1}) {
+function sendMqttinit({roomName}) {
+    // const splitedTopic = topic.split('/');
     const options={
-        clientId: clientId,
+        clientId: process.env.MQTT_CLIENT,
+        clean: false
+        // username:"stev",
+        // password:"passwrd",
+    };
+    
+    var options_publish={
+        retain: true,
+        qos: 1
+    };
+
+    const data_add = "Hellow " + roomName + " Wellcom. You can get add updates...";
+    const data_delete = "Hellow " + roomName + " Wellcom. You can get delete updates...";
+    const topic_add = roomName + '/' + 'add';
+    const topic_delete = roomName + '/' + 'delete';
+    // const topic_list = [topic_add, topic_delete];
+    const tokenizeData_add =  jwt.sign(data_add, process.env.MQTT);
+    const tokenizeData_delete =  jwt.sign(data_delete, process.env.MQTT);
+ 
+    const client = mqtt.connect("mqtt://test.mosquitto.org",options);
+    console.log("connected flag  ", roomName);
+    client.on("connect", function(){	
+        console.log("connected  "+client.connected);
+        if (client.connected==true){
+            // setInterval(function(){
+                client.publish(topic_add, tokenizeData_add, options_publish, ()=>{
+                
+                    console.log("Retain Masseage published to adding event");
+                    client.end();
+
+                });
+
+                client.publish(topic_delete, tokenizeData_delete, options_publish, ()=>{
+                
+                    console.log("Retain Masseage published delete event");
+                    client.end();
+
+                })
+            // ;},3000);
+        }
+    });
+
+    client.on("error",function(error){
+        console.log("Can't connect" + error);
+        process.exit(1)
+    });
+}
+
+function sendMqttNew({data, topic, isretain = false, isclean = false, qosLevel = 1}) {
+    const splitedTopic = topic.split('/');
+    const options={
+        clientId: process.env.MQTT_CLIENT,
         clean: isclean
         // username:"stev",
         // password:"passwrd",
@@ -18,7 +70,7 @@ function sendMqttNew({data, clientId, isretain = false, isclean = false, qosLeve
         qos: 1
     };
 
-    const topicPublish = clientId;
+    // const topicPublish = clientId;
     const tokenizeData =  jwt.sign(data, process.env.MQTT);
  
     const client = mqtt.connect("mqtt://test.mosquitto.org",options);
@@ -26,13 +78,14 @@ function sendMqttNew({data, clientId, isretain = false, isclean = false, qosLeve
     client.on("connect", function(){	
         console.log("connected  "+client.connected);
         if (client.connected==true){
-            setInterval(function(){
-                client.publish(topicPublish, tokenizeData, options_publish, ()=>{
+            // setInterval(function(){
+                client.publish(topic, tokenizeData, options_publish, ()=>{
                 
                 console.log("Masseage published");
-                // client.end();
+                client.end();
 
-            });},3000);
+            })
+            // ;},3000);
         }
     });
 
@@ -42,5 +95,5 @@ function sendMqttNew({data, clientId, isretain = false, isclean = false, qosLeve
     });
 }
 
-module.exports = sendMqttNew;
+module.exports = {sendMqttNew, sendMqttinit};
 // const cli = sendMqttNew('01311', 'cli01');
--- localServer/subscriber.js
+++ localServer/subscriber.js
@@ -1,23 +1,38 @@
 const mqtt = require('mqtt');
 const jwt = require('jsonwebtoken');
 
-var topic_s="cli01";
+function subscribeClient({roomName}) {
+	var topic_add = roomName + "/add";
+	var topic_delete = roomName + "/delete";
 
-const client = mqtt.connect("mqtt://localhost:9999",options);
-console.log("connected flag  "+client.connected);
-client.on("connect",function(){	
-    console.log("connected  "+client.connected);
-	client.subscribe(topic_s,{qos:1});
-});
+	const options={
+		clientId: roomName,
+		clean: false
+		// username:"stev",
+		// password:"passwrd",
+	};
 
-function decodemsg(token) {
-	const decoded = jwt.verify(token, 123);
-	return decode;
+	const client = mqtt.connect("mqtt://test.mosquitto.org",options);
+	// console.log("connected flag  "+client.connected);
+	client.on("connect",function(){	
+		console.log("connected  "+client.connected);
+		client.subscribe([topic_add, topic_delete], {qos:1});
+		console.log("sebscribed your events",roomName);
+		// client.subscribe(topic_s1,{qos:1});
+	});
+
+	function decodemsg(token) {
+		const decoded = jwt.verify(token, '123');
+		return decoded;
+		
+	}
+
+	client.on('message',function(topic, message, packet){
+		// console.log("message  " + message);
+		console.log("message decoded " + decodemsg(message.toString()));
+		console.log("topic is " + topic);
+	});
 	
 }
 
-client.on('message',function(topic, message, packet){
-	console.log("message  "+ message);
-	console.log("message decoded "+ decodemsg(message));
-	console.log("topic is "+ topic);
-});
+subscribeClient({roomName:'room0'});
